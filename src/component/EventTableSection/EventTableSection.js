10c03f4c0fe18316b7beaadff12555a58546780b
nguyenanhdung191 <nguyenanhdung191@gmail.com>
2018-05-28 17:01:05 +0700

    re-indent

--- src/component/EventTableSection/EventTableSection.js
+++ src/component/EventTableSection/EventTableSection.js
@@ -9,101 +9,101 @@ import moment from "moment";
 import "./EventTableSection.css";
 
 export default class EventTableSection extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      events: []
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			events: []
+		};
+	}
 
-  generateEventRows = () => {
-    return this.props.events.map(event => {
-      let rowClassName = "";
-      let tooltip = "";
-      switch (event.syncStatus) {
-        case "Synced":
-          rowClassName = "synced";
-          tooltip = (
-            <div className="tooltip-container">
-              Synced date: {moment(event.syncedDate).format("YYYY-MM-DD")}
-            </div>
-          );
-          break;
-        case "Pendding":
-          rowClassName = "pending";
-          break;
-        case "Rejected":
-          rowClassName = "rejected";
-          tooltip = (
-            <div className="tooltip-container">
-              Rejected reason: {event.syncErrorMessage}
-            </div>
-          );
-          break;
-        default:
-          break;
-      }
+	generateEventRows = () => {
+		return this.props.events.map(event => {
+			let rowClassName = "";
+			let tooltip = "";
+			switch (event.syncStatus) {
+				case "Synced":
+					rowClassName = "synced";
+					tooltip = (
+						<div className="tooltip-container">
+							Synced date: {moment(event.syncedDate).format("YYYY-MM-DD")}
+						</div>
+					);
+					break;
+				case "Pendding":
+					rowClassName = "pending";
+					break;
+				case "Rejected":
+					rowClassName = "rejected";
+					tooltip = (
+						<div className="tooltip-container">
+							Rejected reason: {event.syncErrorMessage}
+						</div>
+					);
+					break;
+				default:
+					break;
+			}
 
-      return (
-        <Tooltip id="tooltip-bottom" title={tooltip} placement="left">
-          <div
-            className={`row ${rowClassName}`}
-            style={{ display: event.showed ? "" : "none" }}
-          >
-            <div className="row-item">
-              <FormControlLabel control={<Checkbox />} label="" />
-            </div>
-            <div className="row-item">{event.eventId}</div>
-            <div className="row-item">{event.eventDate}</div>
-            <div className="row-item">{event.orgUnit}</div>
-            <div className="row-item">{event.district}</div>
-            <div className="row-item">{event.age}</div>
-            <div className="row-item">{event.gender}</div>
-            <div className="row-item">{event.result}</div>
-            <div className="row-item">{event.treatment}</div>
-          </div>
-        </Tooltip>
-      );
-    });
-  };
+			return (
+				<Tooltip id="tooltip-bottom" title={tooltip} placement="left">
+					<div
+						className={`row ${rowClassName}`}
+						style={{ display: event.showed ? "" : "none" }}
+					>
+						<div className="row-item">
+							<FormControlLabel control={<Checkbox />} label="" />
+						</div>
+						<div className="row-item">{event.eventId}</div>
+						<div className="row-item">{event.eventDate}</div>
+						<div className="row-item">{event.orgUnit}</div>
+						<div className="row-item">{event.district}</div>
+						<div className="row-item">{event.age}</div>
+						<div className="row-item">{event.gender}</div>
+						<div className="row-item">{event.result}</div>
+						<div className="row-item">{event.treatment}</div>
+					</div>
+				</Tooltip>
+			);
+		});
+	};
 
-  render() {
-    return (
-      <div className="event-table-container">
-        <div>
-          <LinearProgress className={this.props.loader} mode="query" />
-        </div>
-        <Paper elevation={1} id="fixed-header">
-          <div className="header-container">
-            <div className="header-item">
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={this.state.checkAll}
-                    onChange={this.handleCheckAll}
-                  />
-                }
-                label=""
-              />
-            </div>
-            <div className="header-item">Event id</div>
-            <div className="header-item">Event date</div>
-            <div className="header-item">Org Unit</div>
-            <div className="header-item">District</div>
-            <div className="header-item">Age</div>
-            <div className="header-item">Gender</div>
-            <div className="header-item">RDT Result</div>
-            <div className="header-item">Treatment</div>
-          </div>
-        </Paper>
-        {this.props.events.length !== 0 ? (
-          <Paper elevation={2} style={{ marginTop: 5 }}>
-            {this.generateEventRows()}
-          </Paper>
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="event-table-container">
+				<div>
+					<LinearProgress className={this.props.loader} mode="query" />
+				</div>
+				<Paper elevation={1} id="fixed-header">
+					<div className="header-container">
+						<div className="header-item">
+							<FormControlLabel
+								control={
+									<Checkbox
+										checked={this.state.checkAll}
+										onChange={this.handleCheckAll}
+									/>
+								}
+								label=""
+							/>
+						</div>
+						<div className="header-item">Event id</div>
+						<div className="header-item">Event date</div>
+						<div className="header-item">Org Unit</div>
+						<div className="header-item">District</div>
+						<div className="header-item">Age</div>
+						<div className="header-item">Gender</div>
+						<div className="header-item">RDT Result</div>
+						<div className="header-item">Treatment</div>
+					</div>
+				</Paper>
+				{this.props.events.length !== 0 ? (
+					<Paper elevation={2} style={{ marginTop: 5 }}>
+						{this.generateEventRows()}
+					</Paper>
+				) : (
+						""
+					)}
+			</div>
+		);
+	}
 }
--- src/util/util.js
+++ src/util/util.js
@@ -1,25 +1,25 @@
 const filterEvent = (events, statusFilter, testResultFilter) => {
-  events.forEach(event => {
-    switch (event.syncStatus) {
-      case "Synced":
-        event.showed =
-          statusFilter.synced && testResultFilter.includes(event.result);
-        break;
-      case "Pendding":
-        event.showed =
-          statusFilter.pending && testResultFilter.includes(event.result);
-        break;
-      case "Rejected":
-        event.showed =
-          statusFilter.rejected && testResultFilter.includes(event.result);
-        break;
-      default:
-        event.showed =
-          statusFilter.noSynced && testResultFilter.includes(event.result);
-        break;
-    }
-  });
-  return events;
+	events.forEach(event => {
+		switch (event.syncStatus) {
+			case "Synced":
+				event.showed =
+					statusFilter.synced && testResultFilter.includes(event.result);
+				break;
+			case "Pendding":
+				event.showed =
+					statusFilter.pending && testResultFilter.includes(event.result);
+				break;
+			case "Rejected":
+				event.showed =
+					statusFilter.rejected && testResultFilter.includes(event.result);
+				break;
+			default:
+				event.showed =
+					statusFilter.noSynced && testResultFilter.includes(event.result);
+				break;
+		}
+	});
+	return events;
 };
 
 export { filterEvent };
